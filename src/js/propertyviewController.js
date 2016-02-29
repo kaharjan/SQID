30d99da0b1e6b2b50809934b1f20e0063d390d2c
Markus Damm <nullachtfuffzn@arcor.de>
2016-02-29 20:18:37 +0100

    adapt propertyview to changes in util.js

--- src/js/propertyviewController.js
+++ src/js/propertyviewController.js
@@ -1,66 +1,45 @@
-classBrowser.factory('PropertyView', function($http, $route, $q){
+classBrowser.factory('PropertyView', function($route, wikidataapi){
+	var RELATED_PROPERTIES_THRESHOLD = 5;
+	
 	var pid;
 	return {
-		getPropertyData: function() {	
-		  var url = buildUrlForApiRequest(pid);
-		  return httpRequest($http, $q, url);
-        },
+		getPropertyData: function() {
+			var url = buildUrlForApiRequest(pid);
+			return httpRequest($http, $q, url);
+		},
+		
 		updatePid: function(){
 			pid = ($route.current.params.id) ? ($route.current.params.id) : "P31";
 		},
+		
 		getPid: function(){
-		  return pid;
+			return pid;
+		},
+		
+		getPropertyData: function() {
+			return wikidataapi.fetchEntityData(pid);
 		}
 	};
 })
-  .controller('PropertyViewController', function($scope, Properties, PropertyView){
+.controller('PropertyViewController', function($scope, Properties, PropertyView, wikidataapi){
 	PropertyView.updatePid();
 	$scope.pid = PropertyView.getPid();
-  	$scope.url = "https://www.wikidata.org/wiki/Property:" + $scope.pid;
+	var numId = $scope.pid.substring(1);
+  $scope.url = "https://www.wikidata.org/wiki/Property:" + $scope.pid;	
 	
-	PropertyView.getPropertyData().then(function(data) {
-		$scope.propertyData = parsePropertyData(data, $scope.pid);
-	  });
+	PropertyView.getPropertyData().then(function(data){
+		console.log(data);
+		$scope.propertyData = wikidataapi.extractEntityData(data, $scope.pid);
+	});
 	
-	
-	Properties.then(function(data){
-  	  $scope.relatedProperties = util.parseRelatedProperties($scope.pid, data.getProperties());
-  	  $scope.propertyNumbers = parsePropertyNumbers(data.getProperties(), $scope.pid);
-	  $scope.datatype = parseDataType(data.getProperties(), $scope.pid);
-  	  console.log("fetched Property");
-  	});
-	
+	Properties.then(function(properties){
+		$scope.datatype = properties.getDatatype(numId);
+		$scope.itemCount = properties.getItemCount(numId);
+		$scope.statementCount = properties.getStatementCount(numId);
+		$scope.qualifierCount = properties.getQualifierCount(numId);
+		$scope.referenceCount = properties.getReferenceCount(numId);
+		$scope.totalCount = $scope.statementCount + $scope.referenceCount + $scope.qualifierCount;
+		$scope.relatedProperties = properties.formatRelatedProperties(properties.getRelatedProperties(numId), PropertyView.RELATED_PROPERTIES_THRESHOLD);
+		console.log("fetched Property");
+	});
 });
-
-function parseDataType(data, pid){
-	return data[pid][util.JSON_DATATYPE];
-}
-
-function parsePropertyNumbers(data, pid){
-	var propertyNumbers = {
-		statements: data[pid][util.JSON_USES_IN_STATEMENTS],
-		items: data[pid][util.JSON_ITEMS_WITH_SUCH_STATEMENTS],
-		statements_qualifiers: data[pid][util.JSON_USES_IN_STATEMENTS_WITH_QUALIFIERS],
-		qualifiers: data[pid][util.JSON_USES_IN_QUALIFIERS],
-		references: data[pid][util.JSON_USES_IN_REFERENCES],
-		properties: data[pid][util.JSON_USES_IN_PROPERTIES],
-		total: 0
-	};
-	propertyNumbers.total = propertyNumbers.statements + propertyNumbers.qualifiers + propertyNumbers.references + propertyNumbers.properties;
-	return propertyNumbers;
-}
-
-function parsePropertyData(data, pid) {
-	var propertyData = {
-		label: "",
-		aliases: "",
-		description: ""
-	}
-	try {
-		propertyData.label = parseLabel(data,pid);
-		propertyData.description = parseDescription(data, pid);
-		propertyData.aliases = parseAliases(data,pid);
-	}
-	catch (e) {}
-	return propertyData;
-}
